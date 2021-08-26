eb618a5306a27ddfdf60e59be4c3051c1676af10
fine <dlgmltjr0925@gmail.com>
2021-08-26 21:59:28 +0900

    kakao 2021 01_new_id

---
+++ programers/kakao_2021_newcomer/01_new_id.js
@@ -0,0 +1,17 @@
+function solution(new_id) {
+  function pad(str) {
+    while (str.length < 3) {
+      str += str[str.length - 1];
+    }
+    return str;
+  }
+
+  let answer = new_id.toLowerCase();
+  answer = answer.replace(/[^a-z\d-_.]/g, "");
+  answer = answer.replace(/\.{2,}/g, ".");
+  answer = answer.replace(/^\.|\.$/g, "");
+  if (answer.length === 0) answer = "a";
+  if (answer.length > 15) answer = answer.slice(0, 15).replace(/\.$/, "");
+
+  return pad(answer);
+}
