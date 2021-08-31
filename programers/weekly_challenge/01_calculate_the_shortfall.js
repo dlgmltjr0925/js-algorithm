ed59b98d12c0bd6257dc23f2dc73240a89e82420
fine <dlgmltjr0925@gmail.com>
2021-08-31 20:11:12 +0900

    weekly challenge 01

---
+++ programers/weekly_challenge/01_calculate_the_shortfall.js
@@ -0,0 +1,12 @@
+function solution(price, money, count) {
+  let answer = 0;
+  let total = 0;
+  
+  for (let i = 0; i < count; i++) {
+      total += price * (i + 1);
+  }
+  
+  const diff = total - money;
+  
+  return diff > 0 ? diff : 0;
+}
