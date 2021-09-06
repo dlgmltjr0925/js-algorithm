916efa7a85ec25e1fbf29f714643bfd94ab6fe7d
fine <dlgmltjr0925@gmail.com>
2021-09-06 19:28:11 +0900

    2021 dev matching lotto rank

---
+++ programers/2021_dev_matching/lotto_rank.js
@@ -0,0 +1,22 @@
+function getRank(matchCount) {
+  if (matchCount > 1) {
+      return 7 - matchCount;
+  } else {
+      return 6;
+  }
+}
+
+function solution(lottos, winNums) {
+  let zeroCount = 0;
+  let matchCount = 0;
+  for (let i = 0; i < lottos.length; i++) {
+      const num = lottos[i];
+      if (num === 0) {
+          zeroCount++;
+      } else if (winNums.includes(num)) {
+          matchCount++;
+      }
+  }
+  
+  return [getRank(matchCount + zeroCount), getRank(matchCount)];
+}
