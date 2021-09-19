ae0a7a0a559012e8b57b292958426763d84b4a64
fine <dlgmltjr0925@gmail.com>
2021-09-20 02:58:50 +0900

    monthly challenge ternary

---
+++ programers/monthly_challenge/ternary.js
@@ -0,0 +1,11 @@
+// 3진법 뒤집기
+function solution(n) {
+  let answer = 0;
+  const ternary = n.toString(3);
+  
+  for (let i = 0; i < ternary.length; i++) {
+      answer += ternary[i] * 3 ** i;
+  }
+  
+  return answer;
+}
