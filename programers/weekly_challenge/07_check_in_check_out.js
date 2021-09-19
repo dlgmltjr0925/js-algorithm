5eee962a3cfc7dc2887090323cd36b8688183461
fine <dlgmltjr0925@gmail.com>
2021-09-19 10:20:29 +0900

    weekly challenge 07

---
+++ programers/weekly_challenge/07_check_in_check_out.js
@@ -0,0 +1,22 @@
+function solution(enter, leave) {
+  const entered = new Set();
+  const answer = Array.from({ length: enter.length }, () => 0);
+  let ei = 0;
+  
+  for (let i = 0; i < leave.length; i++) {
+      const number = leave[i];
+      
+      while (!entered.has(number)) {
+          entered.add(enter[ei++]);
+      }
+      
+      entered.delete(number);
+      s
+      answer[number - 1] += entered.size;
+      entered.forEach(number => {
+          answer[number - 1]++;
+      });
+  }
+  
+  return answer;
+}
