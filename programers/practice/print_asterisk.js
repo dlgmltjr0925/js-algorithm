f12cac3f5d6079ca207a8d247458bf81929bc684
fine <dlgmltjr0925@gmail.com>
2021-09-20 15:47:42 +0900

    practice print asterisk

---
+++ programers/practice/print_asterisk.js
@@ -0,0 +1,15 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    let answer = '';
+    let row = '';
+    for (let i = 0; i < a; i++) {
+        row += '*';
+    }
+    row += '\n';
+    for (let i = 0; i < b; i++) {
+        answer += row;
+    }
+    console.log(answer);
+});
