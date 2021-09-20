e4768a8a1839e5937ab72b662d2796feb358bac1
fine <dlgmltjr0925@gmail.com>
2021-09-20 10:18:29 +0900

    practice 2016 year

---
+++ programers/practice/2016_year.js
@@ -0,0 +1,10 @@
+const daysOfMonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const weeks = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
+
+function solution(a, b) {
+    let days = b;
+    for (let i = 0; i < a; i++) {
+        days += daysOfMonth[i];
+    }
+    return weeks[days % 7];
+}
