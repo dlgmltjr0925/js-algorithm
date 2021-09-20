7ac16625cbf7a0d8022042de9159fc23838312c3
fine <dlgmltjr0925@gmail.com>
2021-09-20 15:35:36 +0900

    practice caesar cipher

---
+++ programers/practice/caesar_cipher.js
@@ -0,0 +1,18 @@
+const lower = 'abcdefghijklmnopqrstuvwxyz';
+const upper = lower.toUpperCase();
+
+function solution(s, n) {
+    let answer = '';
+    
+    for (const c of s) {
+        if (/[a-z]/.test(c)) {
+            answer += lower[(c.charCodeAt() + n) % 97 % 26];
+        } else if (/[A-Z]/.test(c)) {
+            answer += upper[(c.charCodeAt() + n) % 65 % 26];
+        } else {
+            answer += ' ';
+        }
+    }
+    
+    return answer;
+}
