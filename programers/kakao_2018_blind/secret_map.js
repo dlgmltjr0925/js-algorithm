9dd66153b28ea689f4b575a3338b0606b3deb576
fine <dlgmltjr0925@gmail.com>
2021-09-20 10:30:08 +0900

    kakao 2018 blind secret map

---
+++ programers/kakao_2018_blind/secret_map.js
@@ -0,0 +1,20 @@
+function solution(n, arr1, arr2) {
+  const answer = [];
+  
+  for (let i = 0; i < n; i++) {
+      let row = '';
+      const map1 = arr1[i].toString(2).padStart(n, '0');
+      const map2 = arr2[i].toString(2).padStart(n, '0');
+      
+      for (let j = 0; j < n; j++) {
+          if (map1[j] === '0' && map2[j] === '0') {
+              row += ' ';
+          } else {
+              row += '#';
+          }
+      }
+      answer.push(row);
+  }
+  
+  return answer;
+}
