09595a33affa4189a3796d10bcefa651d36fc8dd
fine <dlgmltjr0925@gmail.com>
2021-09-20 10:07:23 +0900

    monthly challenge sum twice

---
+++ programers/monthly_challenge/sum_twice.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  const answer = {};
+  numbers.sort((a, b) => a - b);
+  
+  for (let i = 0; i < numbers.length - 1; i++) {
+      for (let j = i + 1; j < numbers.length; j++) {
+          answer[numbers[i] + numbers[j]] = true;
+      }
+  }
+  
+  return Object.keys(answer).map(str => parseInt(str));
+}
