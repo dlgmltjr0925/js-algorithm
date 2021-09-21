f82b5d45af5c5a7ae08f8a5e551fff96fcbe2836
fine <dlgmltjr0925@gmail.com>
2021-09-21 16:14:52 +0900

    practice array lcm

---
+++ programers/practice/array_lcm.js
@@ -0,0 +1,21 @@
+/**
+ * N개의 최소 공배수
+ * https://myjamong.tistory.com/138
+ * 유클리드 호제법(Euclidean Algorithm)으로 gcm(최대공약수)을 구한다
+ * lcm(최소공배수) = 두 자연수의 곱 / gcm 공식을 사용하여 lcm을 구한다.
+ */
+function getGcd(a, b) {
+  if (b === 0) return a;
+  else return getGcd(b, a % b);
+}
+
+function solution(arr) {
+  let answer = arr[0];
+  
+  for (let i = 1; i < arr.length; i++) {
+      const gcd = getGcd(answer, arr[i]);
+      answer= answer * arr[i] / gcd;
+  }
+  
+  return answer;
+}
