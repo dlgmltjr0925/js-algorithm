7c49bc69dffbcd6e3c550d6b09d1c6da2d6bc0d5
fine <dlgmltjr0925@gmail.com>
2021-09-22 18:39:08 +0900

    포맷 수정

--- programers/kakao_2019_intern/tuple.js
+++ programers/kakao_2019_intern/tuple.js
@@ -1,14 +1,15 @@
 function solution(s) {
   const answer = [];
-  const arr = s.slice(2, s.length - 2)
-      .split('},{')
-      .sort((a, b) => a.length - b.length);
-  
+  const arr = s
+    .slice(2, s.length - 2)
+    .split('},{')
+    .sort((a, b) => a.length - b.length);
+
   for (const e of arr) {
-      e.split(',').forEach(v => {
-          if (!answer.includes(v)) answer.push(v);
-      })
+    e.split(',').forEach((v) => {
+      if (!answer.includes(v)) answer.push(v);
+    });
   }
-  
-  return answer.map(v => parseInt(v));
+
+  return answer.map((v) => parseInt(v));
 }
--- programers/kakao_2020_intern/maximum_expression.js
+++ programers/kakao_2020_intern/maximum_expression.js
@@ -1,56 +1,56 @@
 function solution(expression) {
   const expressions = [];
-  
+
   let num = '';
   for (const c of expression) {
-      if (/\D/.test(c)) {
-          expressions.push(parseInt(num));
-          num = '';
-          expressions.push(c);
-      } else {
-          num += c;
-      }
+    if (/\D/.test(c)) {
+      expressions.push(parseInt(num));
+      num = '';
+      expressions.push(c);
+    } else {
+      num += c;
+    }
   }
   expressions.push(parseInt(num));
-  
+
   let answer = 0;
   const operations = ['*', '+', '-'];
-  
+
   function calculate(order) {
-      let exps = [...expressions];
-      
-      for (let i = 0; i < 3; i++) {
-          const exp = order[i];
-          const stack = [];
-          
-          while (exps.length) {
-              const e = exps.shift();
-              if (!isNaN(e) || e !== exp) {
-                  stack.push(e);
-              } else {
-                  const prev = stack.pop();
-                  const next = exps.shift();
-                  stack.push(eval(`${prev}${e}${next}`));
-              }
-          }
-          exps = stack;
+    let exps = [...expressions];
+
+    for (let i = 0; i < 3; i++) {
+      const exp = order[i];
+      const stack = [];
+
+      while (exps.length) {
+        const e = exps.shift();
+        if (!isNaN(e) || e !== exp) {
+          stack.push(e);
+        } else {
+          const prev = stack.pop();
+          const next = exps.shift();
+          stack.push(eval(`${prev}${e}${next}`));
+        }
       }
-      
-      return Math.abs(exps[0]);
+      exps = stack;
+    }
+
+    return Math.abs(exps[0]);
   }
-  
+
   function dfsOrder(order) {
-      if (order.length === 3) {
-          answer = Math.max(answer, calculate(order));
-      } else {
-          for (const operation of operations) {
-              if (order.includes(operation)) continue;
-              dfsOrder([...order, operation]);
-          }
+    if (order.length === 3) {
+      answer = Math.max(answer, calculate(order));
+    } else {
+      for (const operation of operations) {
+        if (order.includes(operation)) continue;
+        dfsOrder([...order, operation]);
       }
+    }
   }
-  
+
   dfsOrder([]);
-  
+
   return answer;
 }
